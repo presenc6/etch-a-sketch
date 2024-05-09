9b2d4051eebf23e4c9b2169de99a3738bf42fcdc
farhan ishraq <154253805+presenc6@users.noreply.github.com>
2024-05-09 16:44:30 +0600

    Added a mouse hovering detection to color divs, added a refresh button

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 const container = document.querySelector(".container");
+const refreshBtn = document.querySelector(".refresh")
 
+const gridSize = prompt("Pick a Grid Size (Maximum: 100)");
+const color = prompt("Pick a Color");
 
-for(let j = 0; j < 400; j++) {
-    const newDiv = document.createElement("div");
-    container.appendChild(newDiv);
-    newDiv.style.width = `${698 / 20}px`;
-    newDiv.style.height = `${698 / 20}px`;
+if(gridSize > 100) {
+    location.reload();
 }
+
+for(let j = 0; j < gridSize * gridSize; j++) {
+    const newDiv = document.createElement("div");
+    newDiv.className = "divs";
+    container.appendChild(newDiv);
+    newDiv.style.width = `${698 / gridSize}px`;
+    newDiv.style.height = `${698 / gridSize}px`;
+}
+
+const divs = document.querySelectorAll(".divs");
+
+divs.forEach(div => {
+    div.addEventListener("mousemove", (e) => {
+        e.target.style.backgroundColor = `${color}`;
+    })
+})
+
+refreshBtn.addEventListener("click", (e) => {
+    location.reload();
+})
 
 
